a53674daca87c586a9f971a7a38015c2b6afed72
Harold C. <h.calio@tagumcooperative.coop>
2023-11-10 11:26:18 +0800

    added github repo link

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
-        <nav>
-            <div class="flex flex-col justify-center h-10 border p-5">
+        <nav class="border p-2 flex flex-row justify-between">
+            <div class="flex flex-col justify-center h-10">
                 <span class="font-bold">JSON-to-Form v.1</span>
+            </div>
+            <div class="flex flex-col justify-center h-10">
+                <NuxtLink to="https://github.com/Roldification/json-to-form" target="_blank"> Github Repo </NuxtLink>
             </div>
         </nav>
         <div class="p-10 flex flex-col items-center">
